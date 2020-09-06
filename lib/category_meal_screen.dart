3e043d6e5647f40c6a96d1613e4a54ae62885ff9
ashrafsalem <eng.ashrafsalem@gmail.com>
2020-09-06 14:27:24 +0200

    adding meal models

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './models/meal.dart';
+import 'dummy_categories.dart';
 
 class CategoryMealScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -13,7 +15,9 @@ class CategoryMealScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgumnets['id'];
     final categoryTitle = routeArgumnets['title'];
-
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -21,7 +25,12 @@ class CategoryMealScreen extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Text('The recipes for the category'),
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length,
+        ),
       ),
     );
   }
